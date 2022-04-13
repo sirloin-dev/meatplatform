ccdcd1b2a28868c11ce883b5b81a60322fee2ec1
Yoonho Chae <72493530+yoonho92@users.noreply.github.com>
2022-04-13 15:54:38 +0900

    Add files via upload

---
+++ study/2022/04-W1/Yoonho.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.sort;
+
+public class Coding {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Coding().solution(new int[]{-1, -2, -3, -4, -5}, -8)));
+    }
+
+    public int[] solution(int[] nums, int target) {
+
+        int[] orig = nums.clone();
+        Arrays.sort(nums);
+        int first = 0;
+        int last = nums.length - 1;
+
+        while (first != last) {
+            if (nums[first] + nums[last] != target) {
+                if (nums[first] + nums[last] > target) last--;
+                else if(nums[first] + nums[last] < target) first++;
+            } else break;
+        }
+
+        for(int i = 0 ; i < orig.length ; i++){
+            if(orig[i] == nums[first]) {
+                first = i;
+                break;
+            }
+        }
+        for(int i = orig.length - 1; 0 < i ; i--){
+            if(orig[i] == nums[last]) {
+                last = i;
+                break;
+            }
+        }
+        return new int[]{first, last};
+    }
+}
