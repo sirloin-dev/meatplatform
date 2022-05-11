edb0bb7d4e74bb8fc767c1b2bea21edb4fb5e695
Yoonho Chae <72493530+yoonho92@users.noreply.github.com>
2022-05-11 13:36:21 +0900

    Add files via upload

---
+++ study/2022/05-W1/Yoonho.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Yoonho {
+    static volatile int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(new Yoonho().longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < strs.length; i++) {
+            String str = strs[i];
+            for (int j = 0; j < str.length(); j++) {
+                String target = str.substring(0, j + 1);
+                if (map.containsKey(target)) {
+                    map.put(target, map.get(target) + 1);
+                } else {
+                    map.put(target, 0);
+                }
+            }
+        }
+
+        String answer = "";
+        int length = 0;
+        for (String key : map.keySet()) {
+            if (map.get(key) == strs.length - 1) {
+                if (length < key.length()) {
+                    length = key.length();
+                    answer = key;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
