ffc7325e770dc7f1aa5348b7944a784fb5a4babf
hong youjin <85492452+howyoujini@users.noreply.github.com>
2022-07-08 15:41:18 +0900

    Update youjin.js

--- study/2022/07-W2/youjin.js
+++ study/2022/07-W2/youjin.js
@@ -4,6 +4,8 @@ const merge = function (nums1, m, nums2, n) {
   return nums1;
 };
 
+// 2022.07.08 금요일 피드백 -> n * logN :: 비효율적이다. + 코드 가독성은 좋다. sort 최적화를 하면 더 좋은 방안이 될 것 같다.
+
 // Success ---
 // Runtime: 71 ms, faster than 79.61% of JavaScript online submissions for Merge Sorted Array.
 // Memory Usage: 41.9 MB, less than 82.63% of JavaScript online submissions for Merge Sorted Array.
