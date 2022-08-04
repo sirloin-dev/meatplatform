b3c1597e22c9420bc71312d8ccf4acee56ac505c
heegon kim <102496472+gon-sirloin@users.noreply.github.com>
2022-08-05 00:27:08 +0900

    Update gon.kt

--- study/2022/08-W1/gon.kt
+++ study/2022/08-W1/gon.kt
@@ -1,17 +1,25 @@
-    class Solution {
-        fun invertTree(root: TreeNode?): TreeNode? = root?.let {
-            inverse(it)
-            root
-        }
+    
+/*
+    1. 왼쪽과 오른쪽 변환
+    2. 왼쪽 재귀호출
+    3. 오른쪽 재귀호출
+    끝
+    null이면 알아서 실행안되니 null경우느 걱정하 필요 없음
+*/
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? = root?.let {
+        inverse(it)
+        root
+    }
 
-        private fun inverse(root: TreeNode?) {
-            root?.let {
-                val a = it.left
-                val b = it.right
-                it.left = b
-                it.right = a
-                inverse(it.left)
-                inverse(it.right)
-            }
+    private fun inverse(root: TreeNode?) {
+        root?.let {
+            val l = it.left
+            val r = it.right
+            it.left = r
+            it.right = l
+            inverse(it.left)
+            inverse(it.right)
         }
     }
+}
