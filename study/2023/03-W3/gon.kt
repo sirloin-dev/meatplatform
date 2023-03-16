2649649f3fa49e534afec6451e1359d411e73b9f
heegon kim <102496472+gon-sirloin@users.noreply.github.com>
2023-03-16 23:10:54 +0900

    Create gon.kt

---
+++ study/2023/03-W3/gon.kt
@@ -0,0 +1,28 @@
+   class Solution {
+
+        fun topKFrequent(nums: IntArray, k: Int): IntArray {
+            val counter : MutableMap<Int,Int> = mutableMapOf()
+
+            // n
+            nums.forEach {
+                counter[it] = counter.getOrDefault(it , 0) + 1
+            }
+
+            val min = counter.values.sortedByDescending { it }.take(k).minOf { it }
+            return counter.entries.filter { it.value >= min }.map { it.key }.toIntArray()
+        }
+
+// leetcode에서 지원으 안하길레 복붙하였습니ㄷ    
+public inline fun <T, R : Comparable<R>> Iterable<T>.minOf(selector: (T) -> R): R {
+    val iterator = iterator()
+    if (!iterator.hasNext()) throw NoSuchElementException()
+    var minValue = selector(iterator.next())
+    while (iterator.hasNext()) {
+        val v = selector(iterator.next())
+        if (minValue > v) {
+            minValue = v
+        }
+    }
+    return minValue
+}
+    }
